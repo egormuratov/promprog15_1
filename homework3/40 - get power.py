9615f4b4a6822b728fd0a794e510fc96c59f8476
Egor Muratov <egormuratov91@gmail.com>
2015-09-21 20:36:04 +0300

    40 - get power.py Done!

--- homework3/40 - get power.py
+++ homework3/40 - get power.py
@@ -5,10 +5,10 @@
 # написать функцию - генератор функций возведения в степень
 
 def get_power(n):
-    def internal(...):
-        return ...
+    def internal(x):
+        return x**n
     return internal
 
 power_5 = get_power(5)
-power_5(2)
+print ('2 ** 5 = '), power_5(2)
 # должен выдать 2 ** 5 = 32
